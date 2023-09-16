31500c3ec48b71b121ac77922928393df2a1d2ed
murad khasmammadov <murad.07mx@gmail.com>
2023-09-16 16:31:41 +0300

    files uploaded

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Jwt_core_demo.Data_Access_Layer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jwt_core_demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Login()
+        {
+            return Created("Token created successfully.", new BuildToken().CreateToken());
+        }
+
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Page()
+        {
+            return Ok("You successfully redirected to Page");
+        }
+
+    }
+}
